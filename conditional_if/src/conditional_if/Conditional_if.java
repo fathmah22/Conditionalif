72020368bf68da862aa84836bbd181b2d1c01a8b
fathmah22 <75178165+fathmah22@users.noreply.github.com>
2021-04-04 12:27:14 +0700

    Add files via upload

---
+++ conditional_if/src/conditional_if/Conditional_if.java
@@ -0,0 +1,20 @@
+package conditional_if;
+
+import java.util.Scanner;
+public class Conditional_if {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int bil;
+        System.out.println("PROGRAM MENENTUKAN BILANGAN HANJIL & GENAP");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.print("Masukan Bilangan = ");
+        bil=input.nextInt();
+        
+        if (bil%2==0){
+            System.out.println("Bilangan "+bil+" merupakan bilangan genap");
+        }else {
+             System.out.println("Bilangan "+bil+" merupakan bilangan ganjil");
+        }
+    
+  }
+}
